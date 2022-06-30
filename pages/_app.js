fe766863ae2c2204bd66cf72eb5e8f2e31a43b3e
sking8484 <sking8484@gmail.com>
2022-06-30 00:38:46 -0400

    added home latex equation

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,12 @@ import Head from 'next/head'
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link
+            href="//cdnjs.cloudflare.com/ajax/libs/KaTeX/0.9.0/katex.min.css"
+            rel="stylesheet"
+        />
+      </Head>
       <Navbar />
       <Component {...pageProps} />
     </>
