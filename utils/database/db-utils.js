f24b06f8789b45c2888f129ea3bd93460dd3cfd6
sking8484 <sking8484@gmail.com>
2022-06-29 23:21:12 -0400

    fixed something

--- utils/database/db-utils.js
+++ utils/database/db-utils.js
@@ -3,5 +3,5 @@ import executeQuery from './db'
 export default async function getAllData(tableName) {
   const query = `SELECT * FROM ${tableName}`;
   var result = await executeQuery({query:query});
-  return result;
+  return result.map(v => Object.assign({}, v));
 }
--- utils/database/db.js
+++ utils/database/db.js
@@ -1,4 +1,5 @@
 import mysql from 'serverless-mysql';
+
 const db = mysql({
   config: {
     host:'162.243.170.65',
@@ -12,11 +13,11 @@ export default async function executeQuery({ query, values}) {
   if (values) {
     let results = await db.query(query, values);
     await db.end();
-    return await results.map(v => Object.assign({}, v));
+    return results
   } else {
     let results = await db.query(query);
     await db.end();
-    return await results.map(v => Object.assign({}, v));
+    return results;
 
   }
 }
