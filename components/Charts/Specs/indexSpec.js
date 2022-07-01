a096987b41f692507c55f2f8bdaf1f13054daeba
sking8484 <sking8484@gmail.com>
2022-07-01 10:48:29 -0400

    reindexing perf

--- components/Charts/Specs/indexSpec.js
+++ components/Charts/Specs/indexSpec.js
@@ -3,12 +3,14 @@ var spec1 = {}
 spec1.mark = 'line';
 spec1.encoding = {
   "x":{"field":"date","timeUnit":"yearmonthdate"},
-  "y":{"field":"Cumulative-Perf","type":"quantitative", "axis":{"format":".0%"}}
+  "y":{"field":"indexedPerf","type":"quantitative", "axis":{"format":".0%"}, "scale":{"zero":false}}
 }
-spec1.transform = [{
-  "window":[{"op":"sum","field":"value", "as":"Cumulative-Perf"}],
+spec1.transform = [{"calculate":"datum.value-(-1)", "as":"posVal"},{
+  "window":[{"op":"product","field":"posVal", "as":"CumulativePerf"},
+    {"op":"first_value","field":"CumulativePerf","as":"FirstIndexVal"}],
   "frame":[null,0]
-}]
+},
+{"calculate":"datum.CumulativePerf/datum.FirstIndexVal - 1", "as":"indexedPerf"}]
 spec1.title = "DeskOne Cumuliative Performance"
 
 spec1.data = {"name":"data"}
