f0dcf20268d119ac3ee0f84634d4d923cbdf6654
sking8484 <sking8484@gmail.com>
2022-07-04 10:04:29 -0400

    gradient for home page

--- components/Charts/Specs/indexSpec.js
+++ components/Charts/Specs/indexSpec.js
@@ -1,11 +1,35 @@
 var spec1 = {}
 
-spec1.mark = 'area';
-spec1.fill = "black"
+spec1.mark = {
+  "type":"area",
+  "line":{
+    "color":"#014f86"
+  },
+  "color": {
+    "x1": 1,
+    "y1": 1,
+    "x2": 1,
+    "y2": 0,
+    "gradient": "linear",
+    "stops": [
+      {
+        "offset": 0,
+        "color": "white"
+      },
+      {
+        "offset": 1,
+        "color": "#014f86"
+      }
+    ]
+  }
+}
 spec1.encoding = {
   "x":{"field":"date","timeUnit":"yearmonthdate"},
   "y":{"field":"indexedPerf","type":"quantitative", "axis":{"format":".0%"}, "scale":{"zero":false}},
   "tooltip":[{"field":"Date (Formatted)"},{"field":"indexedPerf", "format":".2%"}],
+
+
+
 
 }
 spec1.transform = [{"calculate":"datum.value-(-1)", "as":"posVal"},{
