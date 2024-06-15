94d2a3cf64718ab55445b907f38dd5a808635e72
sking8484 <sking8484@gmail.com>
2024-06-15 14:45:21 -0400

    changing database

--- utils/database/db.js
+++ utils/database/db.js
@@ -1,23 +1,44 @@
-import mysql from 'serverless-mysql';
+const SocksConnection = require('socksjs');
+const mysql = require('mysql2');
+const fixieUrl = process.env.FIXIE_SOCKS_HOST;
+const fixieValues = fixieUrl.split(new RegExp('[/(:\\/@)/]+'));
 
-const db = mysql({
-  config: {
-    host:'162.243.170.65',
-    database:'liveDatabase',
-    user:'lebesgel',
-    password:'h3MF$DLNag?QEF!s'
-  }
+const mysqlServer = {
+  host: process.env.DB_HOST,
+  port: 3306
+};
+
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const db = process.env.DB_DB;
+
+const fixieConnection = new SocksConnection(mysqlServer, {
+  user: fixieValues[0],
+  pass: fixieValues[1],
+  host: fixieValues[2],
+  port: fixieValues[3],
 });
 
-export default async function executeQuery({ query, values}) {
+const mysqlConnPool = mysql.createPool({
+  user: dbUser,
+  password: dbPassword,
+  database: db,
+  stream: fixieConnection
+});
+
+export default function executeQuery({ query, values}) {
+  mysqlConnPool.getConnection(function gotConnection(err, connection) {
+
+  if (err) throw err;
+
   if (values) {
-    let results = await db.query(query, values);
-    await db.end();
+    let results = connection.query(query, values);
+    connection.release();
     return results
   } else {
-    let results = await db.query(query);
-    await db.end();
+    let results = await connectionn.query(query);
+    connection.release();
     return results;
-
   }
+}
 }
