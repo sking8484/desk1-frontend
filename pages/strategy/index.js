38e37c7b3402fcff8510fce61e695c96df5956f3
sking8484 <sking8484@gmail.com>
2024-06-18 23:35:42 -0400

    stuff

--- pages/strategy/index.js
+++ pages/strategy/index.js
@@ -11,15 +11,14 @@ import styles from './index.module.css'
 
 
 let dataInfoTitle = 'Retrieve Data'
-let dataInfoText = 'Each day, our rebalancing process begins with data collection. Our universe is the S&P100, so we get updated pricing for each of those stocks. We also compile a number of economic variables (GDP, CPI, etc.) in order to give our algorithm a way to identify the current market enviornment.'
+let dataInfoText = 'Each day, our rebalancing process begins with data collection. Our universe is the S&P100, so we get updated pricing for each of those stocks.'
 
 let networkInfoTitle = 'Price Predictions'
-let networkInfoText1 = `Once we've recieved the data, we use a previously trained Recurrent Neural Network to make return predictions on each of the S&P100 companies.`
-let networkInfoText2 = `At the end of each week, we take the new data that we've recieved through out the week and retrain the Neural Network. This way, the network continuously learns the current enviornment, and how it is different / similar to previous enviornments`
+let networkInfoText1 = `Once we've recieved the data, we use an a prediction process that employes a technique called Multivariate Singular Specturm Analysis. This works by using a Singular Value Decomposition process to filter out unimportant price movements.`
 
 
 let optimizationTitle = 'Portfolio Optimization'
-let optimizationText1 = 'We then pass the output of the Neural Network, along with a covariance matrix associated with the previous year daily price movements of the S&P100 stocks into our optimization program.'
+let optimizationText1 = 'We then pass the output of the MSSA, along with a covariance matrix associated with the previous year daily price movements of the S&P100 stocks into our optimization program.'
 let optimizationText2 = 'This program utilizes Quadratic Programming to find the set of weights that give an optimal risk-return trade off for a certain risk threshold.'
 
 let optimizationText3 = 'We currently bound our weights between 0 and 10%, meaning no short-selling and no over concentration of a single security.'
@@ -37,7 +36,7 @@ export default function Home() {
   const netWorkInfo = {
     'id':'netWorkInfo',
     'title':networkInfoTitle,
-    'text':[networkInfoText1, networkInfoText2],
+    'text':[networkInfoText1],
     'side':'right'
   }
   const optimizationInfo = {
